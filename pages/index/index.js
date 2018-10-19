9a71203ddf4576a739c452a539f917f38ec3f228
zqc <172232502@qq.com>
2018-10-19 23:11:16 +0800

    Initial Commit

---
+++ pages/index/index.js
@@ -0,0 +1,156 @@
+const weatherMap = {
+  'sunny': '晴天',
+  'cloudy': '多云',
+  'overcast': '阴',
+  'lightrain': '小雨',
+  'heavyrain': '大雨',
+  'snow': '雪'
+}
+const weatherColorMap = {
+  'sunny': '#cbeefd',
+  'cloudy': '#deeef6',
+  'overcast': '#c6ced2',
+  'lightrain': '#bdd5e1',
+  'heavyrain': '#c5ccd0',
+  'snow': '#aae1fc'
+}
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    nowTemp: '',
+    nowWeather: '',
+    nowWeatherBackground: '',
+    hourlyWeather: [],
+    todayTemp: "",
+    todayDate: ""
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+    this.getNow()
+  },
+
+  getNow: function(callback) {
+    wx.request({
+      url: 'https://test-miniprogram.com/api/weather/now', //仅为示例，并非真实的接口地址
+      data: {
+        city: '广州市'
+      },
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: res => {
+        let result = res.data.result
+        this.setNow(result)
+        this.setHourlyWeather(result)
+        this.setToday(result)
+      },
+      complete: () => {
+        callback && callback()
+      }
+    })
+  },
+
+  setNow(result) {
+    let temp = result.now.temp
+    let weather = result.now.weather
+    console.log(temp, weather)
+    this.setData({
+      nowTemp: temp,
+      nowWeather: weatherMap[weather],
+      nowWeatherBackground: '/images/' + weather + '-bg.png'
+    })
+    wx.setNavigationBarColor({
+      frontColor: '#ffffff',
+      backgroundColor: weatherColorMap[weather]
+    })
+  },
+
+  setHourlyWeather(result) {
+    // set forecast
+    let forecast = result.forecast
+    let nowHour = new Date().getHours()
+    let hourlyWeather = []
+    for (let i = 0; i < 8; i++) {
+      hourlyWeather.push({
+        time: (i * 3 + nowHour) % 24 + '时',
+        iconPath: '/images/' + forecast[i].weather + '-icon.png',
+        temp: forecast[i].temp + '°'
+      })
+    }
+    hourlyWeather[0].time = '现在'
+    this.setData({
+      hourlyWeather: hourlyWeather
+    })
+  },
+
+  setToday(result) {
+    let date = new Date()
+    this.setData({
+      todayTemp: `${result.today.minTemp}° - ${result.today.maxTemp}°`,
+      todayDate: `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()} 今天`
+    })
+  },
+  onTapDayWeather() {
+    wx.navigateTo({
+      url: '/pages/list/list'
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+    this.getNow(() => {
+      wx.stopPullDownRefresh()
+    })
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  }
+})
---
+++ pages/list/list.js
@@ -0,0 +1,41 @@
+const dayMap = ['星期天', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六']
+// pages/list/list.js
+Page({
+  data: {
+    weekWeather: []
+  },
+  onLoad() {
+    this.getWeekWeather()
+  },
+  getWeekWeather() {
+    wx.request({
+      url: 'https://test-miniprogram.com/api/weather/future',
+      data: {
+        time: new Date().getTime(),
+        city: '广州市'
+      },
+      success: res => {
+        let result = res.data.result
+        // console.log(result)
+        this.setWeekWeather(result)
+      }
+    })
+  },
+  setWeekWeather(result) {
+    let weekWeather = []
+    for (let i = 0; i < 7; i++) {
+      let date = new Date()
+      date.setDate(date.getDate() + i)
+      weekWeather.push({
+        day: dayMap[date.getDay],
+        date: `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`,
+        temp: `${result[i].minTemp}°-${result[i].maxTemp}°`,
+        iconPath: '/images/' + result[i].weather + '-icon.png'
+      })
+    }
+    weekWeather[0].time = '今天'
+    this.setData({
+      weekWeather: weekWeather
+    })
+  }
+})
